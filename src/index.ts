296daa11b1c56002139953c3d9c6f253e1d7826b
Darren <fangweichengvg@gmail.com>
2025-02-13 07:47:19 +0800

    简单学习了一下&来做true的强标记，还有一些不明白。

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,22 @@ export type BillInput = {//导出，类型，账单输入
 }
 
 type billItem = SharedBillItem | PersonalBillItem
-//类型，账单项目是分享账单项目或是个人账单项目
-//两种不同的账单格式合并成一种通用格式的方法
+  //类型，账单项目是分享账单项目或是个人账单项目
+  //两种不同的账单格式合并成一种通用格式的方法
 
 type CommonBillItem = {//类型，常见的账单项目，下列分类数据类型
-price : number //价钱，数字类型
-name : string //名字，字串类型
+  price : number //价钱，数字类型
+  name : string //名字，字串类型
 
 }
+
+type SharedBillItem = CommonBillItem & {//类型，分享账单项目与常见账单项目结合
+// 使用‘&’将共享订单确定一个标记，明确共享账单里，必须带有isShared:true
+  isShared: true
+//类似强标签的意思，若没带true，类型就会不匹配，
+//意思就像，我买雪糕必须冷藏，告诉程序，这个账单必须按照共享的规则处理。
+}
+
 
 function formatDate(date: string): string {
   const [year, month, day] = date.split('-');//使用‘-’分隔开字串，取得年月日。
